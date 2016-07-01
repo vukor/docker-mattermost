533860ba8ebb51b9b5c2dff5ae3227d5d8392150
Anton Bugreev <anton@bugreev.ru>
2016-07-01 13:31:58 +0700

    Поправил './manage.sh test'

--- manage.sh
+++ manage.sh
@@ -93,7 +93,7 @@ case $1 in
     ;;
 	
   test )
-    docker run --net=mattermost-net --rm vukor/mattermost:test /test.sh
+    docker run --net=mattermost-net --rm vukor/mattermost /test.sh
     if [ $? != 0 ]; then exit 1; fi
     ;;
 
